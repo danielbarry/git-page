0594643f190751f06834afc11c23c500afd960cd
Dan <danbarry16@googlemail.com>
2020-05-22 20:18:42 +1200

    Server process page generation

--- src/Server.java
+++ src/Server.java
@@ -16,6 +16,7 @@ public class Server extends Thread{
 
   private ServerSocket ss;
   private Socket s;
+  private PageBuilder pb;
 
   /**
    * Server()
@@ -23,8 +24,9 @@ public class Server extends Thread{
    * Initialise the server.
    *
    * @param port The port number to start the server on.
+   * @param repos The directories of the repositories of interest.
    **/
-  public Server(int port){
+  public Server(int port, String[] repos){
     try{
       ss = new ServerSocket(port);
     }catch(IOException e){
@@ -32,6 +34,7 @@ public class Server extends Thread{
       ss = null;
     }
     s = null;
+    pb = new PageBuilder(repos);
   }
 
   /**
@@ -40,10 +43,12 @@ public class Server extends Thread{
    * Initialise the server socket handler thread.
    *
    * @param socket The socket to be handled by the thread.
+   * @param pageBuilder Access to the page builder object.
    **/
-  public Server(Socket socket){
+  public Server(Socket socket, PageBuilder pageBuilder){
     ss = null;
     s = socket;
+    pb = pageBuilder;
   }
 
   /**
@@ -56,7 +61,7 @@ public class Server extends Thread{
     for(;;){
       try{
         /* Block until a connection is made */
-        (new Server(ss.accept())).start();
+        (new Server(ss.accept(), pb)).start();
       }catch(Exception e){
         Main.warn("Server main loop crashed, but recovered");
       }
@@ -82,22 +87,24 @@ public class Server extends Thread{
       os = s.getOutputStream();
       /* Send the header early */
       os.write(HTTP_HEAD);
-    }catch(IOException e){
-      is = null;
-      os = null;
-    }
-    /* TODO: Handle the client rapidly. */
-    try{ os.write("<h1>test</h1>".getBytes()); }catch(IOException e){} // TODO
-    /* Skip rest of input (up to a maximum) */
-    try{
+      /* Get the request */
+      byte[] buff = new byte[256];
+      is.read(buff);
+      String req = new String(buff);
+      int reqA = req.indexOf(' ') + 1;
+      int reqB = req.indexOf(' ', reqA);
+      if(reqA != reqB && reqB > 0){
+        req = req.substring(reqA, reqB);
+      }else{
+        req = "?";
+      }
+      /* Generate a new page */
+      pb.generate(os, req);
+      /* Skip rest of input (up to a maximum) */
       int a = is.available();
       a = a < 65535 ? a : 65535;
       is.skip(a);
-    }catch(IOException e){
-      /* Do nothing */
-    }
-    /* Close the connection */
-    try{
+      /* Close the connection */
       os.flush();
       s.close();
     }catch(IOException e){
