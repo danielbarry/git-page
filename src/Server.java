adfef2f6d9043d3df74176fdb9e16636e6ca33dd
Dan <danbarry16@googlemail.com>
2020-05-28 00:10:44 +1200

    Derive server magic values from configuration file

--- src/Server.java
+++ src/Server.java
@@ -12,10 +12,9 @@ import java.net.Socket;
  * Run the server indefinitely and serve pages.
  **/
 public class Server extends Thread{
-  private static final int HTTP_HEAD_SIZE = 256;
-  private static final int HTTP_MAX_INPUT = 65536;
-  private static final int HTTP_MAX_WAIT = 5000;
-
+  private int headSize;
+  private int maxInput;
+  private int maxWait;
   private ServerSocket ss;
   private Socket s;
   private PageBuilder pb;
@@ -28,14 +27,36 @@ public class Server extends Thread{
    * @param config Access to the configuration data.
    **/
   public Server(JSON config){
-    int port = -1;
-    /* Try to get a port number */
-    if(config != null && config.get("port") != null && config.get("port").value() != null){
-      try{
-        port = Integer.parseInt(config.get("port").value());
-      }catch(NumberFormatException e){
-        port = -1;
+    /* Set sane defaults */
+    int port = 8080;
+    headSize = 256;
+    maxInput = 65536;
+    maxWait = 5000;
+    /* Try to get server settings */
+    if(config.get("server") != null){
+      JSON sConfig = config.get("server");
+      /* Try to set port */
+      if(sConfig.get("port") != null && sConfig.get("port").value() != null){
+        port = Integer.parseInt(sConfig.get("port").value());
+        Main.log("Port set to '" + port + "'");
       }
+      /* Try to set head size */
+      if(sConfig.get("head-size") != null && sConfig.get("head-size").value() != null){
+        headSize = Integer.parseInt(sConfig.get("head-size").value());
+        Main.log("Head size set to '" + headSize + "'");
+      }
+      /* Try to set max input */
+      if(sConfig.get("max-input") != null && sConfig.get("max-input").value() != null){
+        maxInput = Integer.parseInt(sConfig.get("max-input").value());
+        Main.log("Max input set to '" + maxInput + "'");
+      }
+      /* Try to set max wait */
+      if(sConfig.get("max-wait-ms") != null && sConfig.get("max-wait-ms").value() != null){
+        maxWait = Integer.parseInt(sConfig.get("max-wait-ms").value());
+        Main.log("Max wait set to '" + maxWait + "'");
+      }
+    }else{
+      Main.warn("No configuration found for server");
     }
     /* Try to start the server */
     try{
@@ -56,11 +77,23 @@ public class Server extends Thread{
    *
    * @param socket The socket to be handled by the thread.
    * @param pageBuilder Access to the page builder object.
+   * @param headSize Maximum head size to be processed for URL request String.
+   * @param maxInput Maximum input to be read from the client socket.
+   * @param maxWait Maximum time to keep the socket around for.
    **/
-  public Server(Socket socket, PageBuilder pageBuilder){
+  public Server(
+    Socket socket,
+    PageBuilder pageBuilder,
+    int headSize,
+    int maxInput,
+    int maxWait
+  ){
     ss = null;
     s = socket;
     pb = pageBuilder;
+    this.headSize = headSize;
+    this.maxInput = maxInput;
+    this.maxWait = maxWait;
   }
 
   /**
@@ -78,7 +111,7 @@ public class Server extends Thread{
     for(;;){
       try{
         /* Block until a connection is made */
-        (new Server(ss.accept(), pb)).start();
+        (new Server(ss.accept(), pb, headSize, maxInput, maxWait)).start();
       }catch(Exception e){
         Main.warn("Server main loop crashed, but recovered");
       }
@@ -97,12 +130,12 @@ public class Server extends Thread{
     OutputStream os = null;
     try{
       /* Make sure we don't get slow loris'd */
-      s.setSoTimeout(HTTP_MAX_WAIT);
+      s.setSoTimeout(maxWait);
       /* Get the streams we'll re-use */
       is = s.getInputStream();
       os = s.getOutputStream();
       /* Get the request */
-      byte[] buff = new byte[HTTP_HEAD_SIZE];
+      byte[] buff = new byte[headSize];
       is.read(buff);
       String req = new String(buff);
       int reqA = req.indexOf(' ') + 1;
@@ -116,7 +149,7 @@ public class Server extends Thread{
       pb.generate(os, req);
       /* Skip rest of input (up to a maximum) */
       int a = is.available();
-      a = a < HTTP_MAX_INPUT ? a : HTTP_MAX_INPUT;
+      a = a < maxInput ? a : maxInput;
       is.skip(a);
       /* Close the connection */
       os.flush();
